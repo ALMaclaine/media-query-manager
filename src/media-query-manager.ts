c32916ed333dc3a68332b84b560da04bc62d5aff
Alessandro Maclaine <almaclaine@gmail.com>
2021-10-13 18:59:54 -0500

    add updateBaseFont

--- src/media-query-manager.ts
+++ src/media-query-manager.ts
@@ -9,50 +9,57 @@ export class MediaQueryManager extends EventTarget {
 
 
     private _handler = () => {
-      for (let i = 0; i < this._watchers.length; i++) {
-          if (this._watchers[i].matches) {
-              this._active = this._breaks[i];
-              break;
-          }
-      }
-      this._changed();
-  }
+        for (let i = 0; i < this._watchers.length; i++) {
+            if (this._watchers[i].matches) {
+                this._active = this._breaks[i];
+                break;
+            }
+        }
+        this._changed();
+    }
 
-  public init(baseFontSize: number) {
-      for (const pt of this._finalBreaks) {
-          const dim = ensureNotPxEm(pt, baseFontSize);
-          const query = `(max-width: ${dim})`;
-          const watcher = window.matchMedia(query);
-          watcher.addEventListener('change', this._handler);
-          this._watchers.push(watcher);
-          this._breaks.push(pt);
-      }
-      this._handler();
-  }
+    public init(baseFontSize: number): void {
+        for (const pt of this._finalBreaks) {
+            const dim = ensureNotPxEm(pt, baseFontSize);
+            const query = `(max-width: ${dim})`;
+            const watcher = window.matchMedia(query);
+            watcher.addEventListener('change', this._handler);
+            this._watchers.push(watcher);
+            this._breaks.push(pt);
+        }
+        this._handler();
+    }
 
-  constructor(breaks: Array<number>, { baseFontSize = 16, delayInit = false } = {}) {
-      super();
-      this._baseFontSize = baseFontSize;
-      this._finalBreaks = [...breaks, 99999];
-      if (!delayInit) {
-          this.init(baseFontSize);
-      }
-  }
+    constructor(breaks: Array<number>, { baseFontSize = 16, delayInit = false } = {}) {
+        super();
+        this._baseFontSize = baseFontSize;
+        this._finalBreaks = [...breaks, 99999];
+        if (!delayInit) {
+            this.init(baseFontSize);
+        }
+    }
 
-  private _changed() {
-      const event = new CustomEvent<{active: number}>('change', { detail: { active: this.active } });
-      this.dispatchEvent(event);
-  }
+    public updateBaseFont(): void {
+        const node = document.querySelector('html');
+        if (node) {
+            node.style.fontSize = `${this._baseFontSize / 16 * 100}%`;
+        }
+    }
 
-  public destroy(): void {
-      this._watchers.forEach(e => e.removeEventListener('change', this._handler));
-  }
+    private _changed() {
+        const event = new CustomEvent<{active: number}>('change', { detail: { active: this.active } });
+        this.dispatchEvent(event);
+    }
 
-  get breaks(): number[] {
-      return this._breaks.sort((a, b) => a - b);
-  }
+    public destroy(): void {
+        this._watchers.forEach(e => e.removeEventListener('change', this._handler));
+    }
 
-  get active(): number {
-      return this._active;
-  }
+    get breaks(): number[] {
+        return this._breaks.sort((a, b) => a - b);
+    }
+
+    get active(): number {
+        return this._active;
+    }
 }
